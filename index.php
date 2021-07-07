ffdfef3beb3323097ca146080723a5bfff5d9bbb
CoderZ90 <62499625+CoderZ90@users.noreply.github.com>
2021-07-07 10:07:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,11 @@
   }
 ?>
 <!DOCTYPE html>
-<!-- Coding By CodingNepal - youtube.com/codingnepal -->
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Download YouTube Video Thumbnail | CodingNepal</title>
+  <title>Download YouTube Video Thumbnail | CodingFire</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 </head>
